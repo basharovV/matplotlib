6f1a387117e2745c119efbdd3660dca665cf1118
Slav <slavbacharov@gmail.com>
2015-04-05 23:03:35 -0400

    More cleanup

--- lib/matplotlib/axes/_axes.py
+++ lib/matplotlib/axes/_axes.py
@@ -3886,10 +3886,9 @@ class Axes(_AxesBase):
         if extent is not None:
             xmin, xmax, ymin, ymax = extent
         else:
-            xmin = np.amin(x)
-            xmax = np.amax(x)
-            ymin = np.amin(y)
-            ymax = np.amax(y)
+            xmin, xmax = (np.amin(x), np.amax(x)) if len(x) else (0, 1)
+            ymin, ymax = (np.amin(y), np.amax(y)) if len(y) else (0, 1)
+
             # to avoid issues with singular data, expand the min/max pairs
             xmin, xmax = mtrans.nonsingular(xmin, xmax, expander=0.1)
             ymin, ymax = mtrans.nonsingular(ymin, ymax, expander=0.1)
@@ -4231,6 +4230,7 @@ class Axes(_AxesBase):
         self.add_collection(q, autolim=True)
         self.autoscale_view()
         return q
+    quiver.__doc__ = mquiver.Quiver.quiver_doc
 
     @docstring.dedent_interpd
     def quiver_shape(self, *args, **kw):
@@ -4300,8 +4300,6 @@ class Axes(_AxesBase):
                         width=width, **kw)
 
         return q
-
-    quiver.__doc__ = mquiver.Quiver.quiver_doc
 
     def stackplot(self, x, *args, **kwargs):
         return mstack.stackplot(self, x, *args, **kwargs)
@@ -5722,12 +5720,14 @@ class Axes(_AxesBase):
 
         # basic input validation
         flat = np.ravel(x)
-        if len(flat) == 0:
-            raise ValueError("x must have at least one data point")
+
+        input_empty = len(flat) == 0
 
         # Massage 'x' for processing.
         # NOTE: Be sure any changes here is also done below to 'weights'
-        if isinstance(x, np.ndarray) or not iterable(x[0]):
+        if input_empty:
+            x = np.array([[]])
+        elif isinstance(x, np.ndarray) or not iterable(x[0]):
             # TODO: support masked arrays;
             x = np.asarray(x)
             if x.ndim == 2:
@@ -5782,7 +5782,7 @@ class Axes(_AxesBase):
         # If bins are not specified either explicitly or via range,
         # we need to figure out the range required for all datasets,
         # and supply that to np.histogram.
-        if not binsgiven:
+        if not binsgiven and not input_empty:
             xmin = np.inf
             xmax = -np.inf
             for xi in x:
@@ -5987,17 +5987,18 @@ class Axes(_AxesBase):
                     if np.sum(m) > 0:  # make sure there are counts
                         xmin = np.amin(m[m != 0])
                         # filter out the 0 height bins
-                xmin = max(xmin*0.9, minimum)
+                xmin = max(xmin*0.9, minimum) if not input_empty else minimum
                 xmin = min(xmin0, xmin)
                 self.dataLim.intervalx = (xmin, xmax)
             elif orientation == 'vertical':
                 ymin0 = max(_saved_bounds[1]*0.9, minimum)
                 ymax = self.dataLim.intervaly[1]
+
                 for m in n:
                     if np.sum(m) > 0:  # make sure there are counts
                         ymin = np.amin(m[m != 0])
                         # filter out the 0 height bins
-                ymin = max(ymin*0.9, minimum)
+                ymin = max(ymin*0.9, minimum) if not input_empty else minimum
                 ymin = min(ymin0, ymin)
                 self.dataLim.intervaly = (ymin, ymax)
 
